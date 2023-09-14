c49a411a094631c2937afcb165c579988b20feab
Yang-soeun <soeun8636@naver.com>
2023-09-14 17:22:56 +0900

    Business Layer 테스트(2)

--- cafekiosk/src/main/java/testcode/cafekiosk/spring/api/service/order/OrderService.java
+++ cafekiosk/src/main/java/testcode/cafekiosk/spring/api/service/order/OrderService.java
@@ -11,6 +11,8 @@ import testcode.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,11 +22,24 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         //Product
-        List<Product> products = productRepository.findAllByProductNumIn(productNumbers);
+        List<Product> products = findProductsBy(productNumbers);
 
+//        Order order = Order.create(products, registeredDateTime);
         Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.of(savedOrder);
+    }
+
+    private List<Product> findProductsBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumIn(productNumbers);//IN 절로 하면 001, 001등 중복되는 상품을 넣은 경우 중복이 제거된다
+
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNum, p -> p));
+
+        List<Product> duplicateProducts = productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
+        return duplicateProducts;
     }
 }
--- cafekiosk/src/test/java/testcode/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ cafekiosk/src/test/java/testcode/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package testcode.cafekiosk.spring.api.service.order;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import testcode.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import testcode.cafekiosk.spring.api.service.order.response.OrderResponse;
+import testcode.cafekiosk.spring.domain.order.OrderRepository;
+import testcode.cafekiosk.spring.domain.orderProduct.OrderProductRepository;
 import testcode.cafekiosk.spring.domain.product.Product;
 import testcode.cafekiosk.spring.domain.product.ProductRepository;
 import testcode.cafekiosk.spring.domain.product.ProductType;
@@ -26,7 +29,14 @@ import static testcode.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 class OrderServiceTest {
     @Autowired private OrderService orderService;
     @Autowired private ProductRepository productRepository;
-
+    @Autowired private OrderRepository orderRepository;
+    @Autowired private OrderProductRepository orderProductRepository;
+    @AfterEach//테스트가 끝날때마다 delete 해줌
+    void tearDown() {
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+    }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
@@ -88,6 +98,38 @@ class OrderServiceTest {
                         tuple("002", 3000)
                 );
 
+    }
+
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers(){
+        //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2= createProduct(HANDMADE, "002", 3000);
+        Product product3= createProduct(HANDMADE, "003", 5000);
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))// 똑같은거 두개 주문하기
+                .build();
+
+        //when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        //then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 2000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000)
+                );
     }
 
     //필요한것만 입력받고 나머지는 그냥 기본값 쓰도록 만들어서 사용하기
