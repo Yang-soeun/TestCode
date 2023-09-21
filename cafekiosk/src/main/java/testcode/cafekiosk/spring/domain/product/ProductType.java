01734b9d3478935d5a131b17725d44e736a2db9d
Yang-soeun <soeun8636@naver.com>
2023-09-21 13:53:00 +0900

    Business Layer 테스트(3)

--- cafekiosk/src/main/java/testcode/cafekiosk/spring/domain/product/ProductType.java
+++ cafekiosk/src/main/java/testcode/cafekiosk/spring/domain/product/ProductType.java
@@ -3,6 +3,8 @@ package testcode.cafekiosk.spring.domain.product;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor //생성자 생성
 public enum ProductType {
@@ -11,4 +13,8 @@ public enum ProductType {
     BAKERY("베이커리");
 
     private final String text; //설명 필드
+
+    public static Boolean containsStockType(ProductType type) {
+        return List.of(BOTTLE, BAKERY).contains(type);
+    }
 }
