c0452b1806f347dedd97d3085d60f08c88b1c5ba
Yang-soeun <soeun8636@naver.com>
2024-05-06 19:01:06 +0900

    [TEST]: @ParameterizedTest - CsvSource 방식

--- cafekiosk/src/test/java/testcode/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ cafekiosk/src/test/java/testcode/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -2,6 +2,8 @@ package testcode.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,4 +35,17 @@ class ProductTypeTest {
         assertThat(result).isTrue();
     }
 
+    /**
+     * Csv 방식
+     */
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @CsvSource({"HANDMADE,false", "BOTTLE,true","BAKERY,true"}) //파라미터 형식이랑 동일하게
+    @ParameterizedTest
+    void containsStockTypee(ProductType productType, boolean expected){
+        //when
+        boolean result = ProductType.containsStockType(productType);
+
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
 }
